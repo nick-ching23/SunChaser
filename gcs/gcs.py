872da8b48cdb9c365ff0d37787fbc1c74eb42499
AveryYay <mf3332@columbia.edu>
2025-02-08 11:26:06 -0500

    Added gcs upload & download features

---
+++ gcs/gcs.py
@@ -0,0 +1,40 @@
+from google.cloud import storage
+
+
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+
+    storage_client = storage.Client(project="sunchaser-450121")
+    print(storage_client.project)
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    generation_match_precondition = 0
+
+    blob.upload_from_filename(source_file_name, if_generation_match=generation_match_precondition)
+
+    print(
+        f"File {source_file_name} uploaded to {destination_blob_name}."
+    )
+
+def download_blob(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a blob from the bucket."""
+
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(bucket_name)
+
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+
+    print(
+        "Downloaded storage object {} from bucket {} to local file {}.".format(
+            source_blob_name, bucket_name, destination_file_name
+        )
+    )
+
+
+bucket_name = "sun-chaser-gcs"
+source_file_name = "../../Mnist.py"
+destination_blob_name = "Mnist.py"
+download_blob(bucket_name, source_file_name, destination_blob_name)
