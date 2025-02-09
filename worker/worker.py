d288e3557a91356d6a36a9b156aac0301fb4c904
WillMa17 <wmasillery@gmail.com>
2025-02-08 20:48:18 -0500

    Some further changes to backend/worker

--- worker/worker.py
+++ worker/worker.py
@@ -1,22 +1,46 @@
 import grpc
 from concurrent import futures
+from gcs import download_blob
 import scheduler_pb2
 import scheduler_pb2_grpc
 import time
 import os
+import shutil
+import subprocess
 
 SCHEDULER_ADDRESS = os.getenv("SCHEDULER_ADDRESS", "192.168.119.1:50052") 
+registry_name = ""
+repo_name = "sunchaser"
 
+def do_task(request):
+    if not os.path.exists('data'):
+        os.makedirs('data')
+    if not os.path.exists(f'data/{request.dataset}'):
+        if os.listdir('data'):
+            shutil.rmtree('data')
+            os.makedirs('data')
+        download_blob("sun-chaser-gcs", request.dataset, f'data/{request.dataset}')
+    docker_img_path = f'{registry_name}/{repo_name}:{request.id}'
+    try:
+        subprocess.run(f"docker pull {docker_img_path}", shell=True, check=True)
+
+        command = f"docker run --rm {docker_img_path} --batch {request.batch} --dataset {request.dataset} 
+                --start {request.start} --end {request.end} 
+                --partitioned {request.partitioned} --output outputfile_{request.id}"
+        subprocess.run(command, shell=True, check=True)
+        return 1
+    except subprocess.CalledProcessError as e:
+        return 0
+    
+    
 class WorkerService(scheduler_pb2_grpc.SchedulerServiceServicer):
-
     def ProcessTask(self, request, context):
         """
         Receives and processes a task from the scheduler.
         """
-
-        #do task
-
-        print(f"Task {request.id} completed!")
+        begin = time.time()
+        do_task(request)
+        end = time.time()
 
         report_status_to_scheduler(worker_name="worker-node", task_id=request.id, status="completed")
 
